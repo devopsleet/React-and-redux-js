75f8cc390ccb51749419680565f45fb9f9336c72
gagan singla <gagansiglabti@outlook.com>
2021-03-09 00:43:42 -0600

    referencing a JS object in JSX

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,20 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function getButtonText(){
-
-    return 'click on me!';
-}
 
 // Create a react component
 const App = () =>{
-const buttonText = 'Click Me!';
+const style = {backgroundColor : 'blue'}; //JS Object
 
 return (
     <div>
         <label for="name">Enter Name</label>
         <input id="name" type="text" />
-        <button style={{backgroundColor:"blue" ,color:"white"}}>{getButtonText()}</button>
+        <button style={style}>click on me!</button> //refrencing an object in JSX
     </div>
 );
 };
