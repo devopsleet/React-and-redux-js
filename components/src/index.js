2ae1b67adc21e25a2ddec46cfaac541f77dd2f84
gasingla <gasingla07@gmail.com>
2021-03-13 11:35:21 -0600

    react components

---
+++ components/src/index.js
@@ -0,0 +1,12 @@
+// import the react libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Create a react component
+const App = () =>{
+
+    return <div>Hi there!</div>
+};
+
+//Take a react component and show it on a screen
+ReactDOM.render(<App />, document.querySelector('#root'));
