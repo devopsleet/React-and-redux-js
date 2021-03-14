43b3723937be9ece7f67c89c8a01546cd50058d0
devopsleet <gasingla07@gmail.com>
2021-03-14 18:07:51 -0500

    Providing props from parent to child

--- components/src/index.js
+++ components/src/index.js
@@ -2,43 +2,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
-import CommonDetail from './CommentDetail';
+import CommentDetail from './CommentDetail';
 
 //Create a react component
 const App = () =>{
 
     return (
         <div className="ui container comment">
+           <CommentDetail author="Gagan" />
            
-           
-            <div className="comment">
-            < a href="/" className="avatar">
-                <img alt="avatar" src={faker.image.image()}/>
-            </a>
-            <div className="content">
-                <a href="/" className="author">
-                    Sam
-                </a>
-                <div className="metadata">
-                    <span className="date">Today at 1PM</span>
-                </div>
-                <div className="text">Nice blog post</div>
-            </div>
-            </div>
-            <div className="comment">
-            < a href="/" className="avatar">
-                <img alt="avatar" src={faker.image.image()}/>
-            </a>
-            <div className="content">
-                <a href="/" className="author">
-                    Sam
-                </a>
-                <div className="metadata">
-                    <span className="date">Today at 1PM</span>
-                </div>
-                <div className="text">Nice blog post</div>
-            </div>
-            </div>
         </div>
     );
 };
