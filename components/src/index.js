a5f44c2f01b594cd5d768d824616c739fb0a55dd
devopsleet <gasingla07@gmail.com>
2021-03-14 15:00:36 -0500

    How to use props in react

--- components/src/index.js
+++ components/src/index.js
@@ -1,11 +1,46 @@
-// import the react libraries
+// import the libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommonDetail from './CommentDetail';
 
 //Create a react component
 const App = () =>{
 
-    return <div>Hiiiii there!</div>
+    return (
+        <div className="ui container comment">
+           
+           
+            <div className="comment">
+            < a href="/" className="avatar">
+                <img alt="avatar" src={faker.image.image()}/>
+            </a>
+            <div className="content">
+                <a href="/" className="author">
+                    Sam
+                </a>
+                <div className="metadata">
+                    <span className="date">Today at 1PM</span>
+                </div>
+                <div className="text">Nice blog post</div>
+            </div>
+            </div>
+            <div className="comment">
+            < a href="/" className="avatar">
+                <img alt="avatar" src={faker.image.image()}/>
+            </a>
+            <div className="content">
+                <a href="/" className="author">
+                    Sam
+                </a>
+                <div className="metadata">
+                    <span className="date">Today at 1PM</span>
+                </div>
+                <div className="text">Nice blog post</div>
+            </div>
+            </div>
+        </div>
+    );
 };
 
 //Take a react component and show it on a screen
